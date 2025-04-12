0eb583c8327ec5c528c632ee07146f8a8811c65d
roma0000 <hishamtaha047@gmail.com>
2025-04-13 00:35:35 +0200

    redeploy1.6

--- server/index.js
+++ server/index.js
@@ -21,32 +21,39 @@ dbConecction();
 //   res.sendStatus(200);
 // });
 
-app.use(
-  cors({
-    origin: "https://prometica.vercel.app",
-    // origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    // allowedHeaders: ["Content-Type"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://prometica.vercel.app",
+//     // origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+//     // allowedHeaders: ["Content-Type"],
+//     credentials: true,
+//   })
+// );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://prometica.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.options("*", cors());
-
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "https://prometica.vercel.app");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   if (req.method === "OPTIONS") {
-//     return res.sendStatus(200);
-//   }
-//   next();
-// });
 
 // app.use(
 //   cors({
 //     origin: "https://prometica.vercel.app",
+//     credentials: true,
+//   })
+// );
+
+// app.use(
+//   cors({
+//     origin: "https://prometica.vercel.app",
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 //     credentials: true,
 //   })
 // );
