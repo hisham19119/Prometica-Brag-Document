e9e1f6b94f5e0a6a59122f3c8b76f0f6243e4533
roma0000 <hishamtaha047@gmail.com>
2025-04-13 22:32:54 +0200

    cors implementation - 3.5.2

--- server/index.js
+++ server/index.js
@@ -9,19 +9,19 @@ const cors = require("cors");
 
 dbConecction();
 
-app.use(
-  cors({
-    origin: "https://prometica.vercel.app",
-    // origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://prometica.vercel.app",
+//     // origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+//     allowedHeaders: ["Content-Type"],
+//     credentials: true,
+//   })
+// );
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://prometica.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Credentials", "true");
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
