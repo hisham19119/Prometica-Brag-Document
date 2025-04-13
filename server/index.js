801766a0b8cca18241d7b6d5d9c95b46893e5b1d
roma0000 <hishamtaha047@gmail.com>
2025-04-13 19:40:13 +0200

    cors implementation - 3.4.3

--- server/index.js
+++ server/index.js
@@ -9,15 +9,15 @@ const cors = require("cors");
 
 dbConecction();
 
-app.use(
-  cors({
-    origin: "https://prometica.vercel.app",
-    // origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    // allowedHeaders: ["Content-Type"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://prometica.vercel.app",
+//     // origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+//     // allowedHeaders: ["Content-Type"],
+//     credentials: true,
+//   })
+// );
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://prometica.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
