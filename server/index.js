5b3a5bad0c0d40431b8576f36a3b6b101aeb5e61
roma0000 <hishamtaha047@gmail.com>
2025-04-13 22:13:07 +0200

    cors implementation - 3.4.9

--- server/index.js
+++ server/index.js
@@ -18,16 +18,16 @@ app.use(
     credentials: true,
   })
 );
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "https://prometica.vercel.app");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   if (req.method === "OPTIONS") {
-//     return res.sendStatus(200);
-//   }
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://prometica.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.options("*", cors());
 // app.use(cors());
